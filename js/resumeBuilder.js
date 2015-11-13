43839ef70cbf413e76c846481ca2cc805b1ae54c
Kyle Ofori <kyle.ofori@detroitlabs.com>
2015-11-13 15:56:19 -0500

    Catching up to Lesson 1 - Data Types (about midway)

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,19 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+var awesomeThoughts;
+
+awesomeThoughts = "I'm Kyle Ofori and I'm AWESOME!";
+
+console.log(awesomeThoughts);
+
+var funThoughts = awesomeThoughts.replace('AWESOME', 'FUN (that\'s weird)');
+// $( "#main" ).append(funThoughts);
+
+var formattedName, formattedRole;
+formattedName = HTMLheaderName.replace("%data%", "Kyle Ofori");
+formattedRole = HTMLheaderRole.replace("%data%", "Mobile Developer");
+
+$("#header").append(formattedName);
+$("#header").append(formattedRole);
